d3db6476956469d0a802de707570639e70eee3f4
Ivan Dovhun <ivan.dovhun@gmail.com>
2021-06-08 19:57:06 +0300

    added services and routers

---
+++ frontend/src/app/property/property-detail/property-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-property-detail',
+  templateUrl: './property-detail.component.html',
+  styleUrls: ['./property-detail.component.scss']
+})
+export class PropertyDetailComponent implements OnInit {
+
+  constructor(private rote: ActivatedRoute) { }
+
+  public propertyId: number;
+
+  ngOnInit() {
+    this.propertyId = this.rote.snapshot.params['id']
+  }
+
+}
